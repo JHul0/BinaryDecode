50e19228f6387533c65adcea42740112cdfb4bb2
Josh <josh@getthepom.com>
2022-09-15 12:25:51 -0400

    cleanup and commenting for clarity

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ var Buffer = require('buffer/').Buffer
 var config = require('config-yml');
 const { error } = require('console');
 
+/************Lambda Handler****************/
 exports.handler = async (event) => {
     let pdecoder = new decoder()
     console.log("encodedData: " + event['encodedData'])
-    const response = pdecoder.decodePacket(event['encodedData'], config.header)
+    const response = pdecoder.decodePacket(event['encodedData'])
     return response;
 };
 
@@ -22,27 +23,48 @@ class decoder{
         this.terminator = 'ff'
     }
 
-    decodePacket(base64Packet, headerConfig){
+    /************Main Function****************/
+    decodePacket(base64Packet){
         let hexPacket = this.convertToHex(base64Packet)
         let [header, remainder] = this.decode(hexPacket, this.packetStruct[0])
         let [dataPacket, notAvailable] = this.decode(remainder, this.packetStruct[header["type"]])
         header[Object.keys(config)[header["type"]]] = dataPacket
         return header
     }
+    /************End Main****************/
 
-    slipDecode(binaryPacket){
-        let result = binaryPacket.replaceAll('8087','3b')
-        result = result.replaceAll('8082','00')
-        result = result.replaceAll('8083','08')
-        result = result.replaceAll('8084','0d')
-        result = result.replaceAll('8085','20')
-        result = result.replaceAll('8086','2c')
-        //this must be the last replace in order to avoid collisions
-        result = result.replaceAll('8081','80')
-        console.log("hex after slip decode: " + result)
-        return result
+    /************Field Data Seperation Logic****************/
+    sliceHex(hexPacket, packetConfig){
+        let result = []
+        let position = 0
+        let i = 0
+        for (let fieldName in packetConfig['fields']){
+            let finalPosition = packetConfig['fields'][fieldName]['size'];
+            if(Number.isInteger(finalPosition)){
+                finalPosition = position + packetConfig['fields'][fieldName]['size']*2
+                result[i] = hexPacket.slice(position, finalPosition)
+                i++
+                position = finalPosition
+            } else if(finalPosition = "variable"){
+                finalPosition = hexPacket.indexOf(this.terminator)
+                result[i] = hexPacket.slice(position, finalPosition)
+
+                //the following line removes the terminator byte for the next loop iteration
+                hexPacket = hexPacket.slice(0, finalPosition-2) + hexPacket.slice(finalPosition)
+
+                //dynamically update the size of the field once the size is known
+                packetConfig['fields'][fieldName]['size'] = (finalPosition-2)-position
+
+                i++
+                position = finalPosition
+            }
+        }
+        console.log("hexSlice: " + result)
+        return [result, hexPacket.slice(position, hexPacket.length)]
     }
+    /************End Data Seperation Logic****************/
 
+    /************Type definitions****************/
     decode(hexPacket, packetConfig){
         let [packetSplitHex, remainderPacket] = this.sliceHex(hexPacket, packetConfig)
         let iotPacket = {}
@@ -77,22 +99,22 @@ class decoder{
                 iotPacket[field] = this.hexToString(packetSplitHex[i])
                 i = i+1
             } else if (packetConfig['fields'][field]['type'] == 'BLEversion'){
-                let rawVersionString = packetSplitHex[i]
-
-                let major = rawVersionString.slice(0, 2)
-                let minor = rawVersionString.slice(2, 4)
-                iotPacket[field] = parseInt(major)+"."+parseInt(minor)
+                iotPacket[field] = this.BLEdecode(packetSplitHex[i])
                 i = i+1
             } else {
-                throw new Error('missed bytes in packet')
+                throw new Error('unspecified field type')
             }
             console.log("\n" + field + ": " + iotPacket[field])
         }
         return [iotPacket, remainderPacket]
     }
+    /************End of Type Definitions****************/
 
-    hexToFloat(hex){
-        const buffer = Buffer.from(hex, 'hex')
+    /************Custom Type Decoding logic****************/
+    BLEdecode(hex){
+        let major = hex.slice(0, 2)
+        let minor = hex.slice(2, 4)
+        return parseInt(major)+"."+parseInt(minor)
     }
 
     hexToString(hex) {
@@ -111,6 +133,20 @@ class decoder{
         return bufString
     }
 
+    parseFloat(str){
+        return Buffer(str,'hex').readFloatLE(0);
+    }
+
+    formatMacID(hexString){
+        hexString = hexString.replace(/(.{2})/g,"$1:")
+        if (hexString.charAt(hexString.length-1) == ':'){
+            hexString = hexString.slice(0,-1)
+        }
+        return hexString
+    }
+    /************End of Custom Type logic****************/
+
+    /************Utilities****************/
     getByteSize(hexPacket){
         return hexPacket.length/2
     }
@@ -123,69 +159,47 @@ class decoder{
         return size
     }
 
-    //todo: return sliced remainder
-    sliceHex(hexPacket, packetConfig){
-        let result = []
-        let position = 0
-        let i = 0
-        for (let fieldName in packetConfig['fields']){
-            let finalPosition = packetConfig['fields'][fieldName]['size'];
-            if(Number.isInteger(finalPosition)){
-                finalPosition = position + packetConfig['fields'][fieldName]['size']*2
-                result[i] = hexPacket.slice(position, finalPosition)
-                i++
-                position = finalPosition
-            } else if(finalPosition = "variable"){
-                finalPosition = hexPacket.indexOf(this.terminator)
-                result[i] = hexPacket.slice(position, (finalPosition))
-                //the following line removes the terminator byte
-                hexPacket = hexPacket.slice(0, finalPosition-2) + hexPacket.slice(finalPosition)
-
-                packetConfig['fields'][fieldName]['size'] = (finalPosition-2)-position
-                i++
-                position = finalPosition
-            }
-        }
-        console.log("hexSlice: " + result)
-        return [result, hexPacket.slice(position, hexPacket.length)]
+    //deprecated: retained for backwards compatibility
+    slipDecode(binaryPacket){
+        let result = binaryPacket.replaceAll('8087','3b')
+        result = result.replaceAll('8082','00')
+        result = result.replaceAll('8083','08')
+        result = result.replaceAll('8084','0d')
+        result = result.replaceAll('8085','20')
+        result = result.replaceAll('8086','2c')
+        //this must be the last replace in order to avoid collisions
+        result = result.replaceAll('8081','80')
+        console.log("hex after slip decode: " + result)
+        return result
     }
-
-    decodeString(hexString){
-        return Buffer.from(hexString, 'hex').toString('utf8')
-    }
-
-   parseFloat(str){
-    return Buffer(str,'hex').readFloatLE(0);
-   }
-
-
-    formatMacID(hexString){
-        hexString = hexString.replace(/(.{2})/g,"$1:")
-        if (hexString.charAt(hexString.length-1) == ':'){
-            hexString = hexString.slice(0,-1)
-        }
-        return hexString
-    }
+    /************End Utilities****************/
 }
 
-//note: data starting with:
-// AQ is a heartbeat
-// Ah is an action
-// Ay is devconfig
+
+/************Local Testing****************/
+
+//notes: 
+//  data starting with:
+//      AQ is a heartbeat
+//      Ah is an action
+//      Ay is devconfig
 
 //legacy event list with slip decoding
-//"AQrFTcN5cbRih8tpMhlPgSYyQDh4Q0KAgoCCgIIZ"
-//"AQrFTcN5cbSAgoCCgIKAgmJxYoCFMiRxgIKAgoCCgIKAgoCCgIKAgoCCgIKAgh0"
+//  "AQrFTcN5cbRih8tpMhlPgSYyQDh4Q0KAgoCCgIIZ"
+//  "AQrFTcN5cbSAgoCCgIKAgmJxYoCFMiRxgIKAgoCCgIKAgoCCgIKAgoCCgIKAgh0"
 
 //event list(no slip):
-//"AyrbilGGUPBjGSWElhlTBAEHBzM3LjAwLjcyNC1QMEMuNzIwMDEx/wEzNTkyMDUxMDI1MDE3NzQBADk="
+//  "AyrbilGGUPBjGSWElhlTBAEHBzM3LjAwLjcyNC1QMEMuNzIwMDEx/wEzNTkyMDUxMDI1MDE3NzQBADk="
+
 
 //uncomment following lines for local testing, replace hex string with the hex string you want to test
 
-//let event = {}
-//event['encodedData'] = "AyrbilGGUPBjGSWElhlTBAEHBzM3LjAwLjcyNC1QMEMuNzIwMDEx/wEzNTkyMDUxMDI1MDE3NzQBADk="
-//let pdecoder = new decoder()
+let event = {}
+event['encodedData'] = "AyrbilGGUPBjGSWElhlTBAEHBzM3LjAwLjcyNC1QMEMuNzIwMDEx/wEzNTkyMDUxMDI1MDE3NzQBADk="
+let pdecoder = new decoder()
 
-//console.log("encodedData: " + event['encodedData'])
-//const response = pdecoder.decodePacket(event['encodedData'], config.header)
-//console.log("\ndecodedObject:\n", response);
+console.log("encodedData: " + event['encodedData'])
+const response = pdecoder.decodePacket(event['encodedData'], config.header)
+console.log("\ndecodedObject:\n", response);
+
+/************End Local Testing****************/
